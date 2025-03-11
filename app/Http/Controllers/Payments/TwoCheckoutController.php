7196778f2eb7a063f38e68ffcb3a6542aab4e624
Xiong Jin <xiong402@gmail.com>
2025-03-12 04:55:14 +0800

    Added 2checkout payment api

---
+++ app/Http/Controllers/Payments/TwoCheckoutController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Payments;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TwoCheckoutController extends Controller
+{
+    public function pay(Request $request)
+    {
+        //
+    }
+}
