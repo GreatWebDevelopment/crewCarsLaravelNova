1315dad90a748b8000f4c367bbee3f581c4f96c0
Xiong Jin <xiong402@gmail.com>
2025-03-14 17:57:58 +0800

    Fixed bookings models

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -11,7 +11,7 @@ class Booking extends Model
     use HasFactory;
     protected $table = 'bookings';
     protected $fillable = [
-        'uid', 'carId', 'totalDayOrHr', 'subtotal', 'type', 'brand', 'rateText', 'cancelReason',
+        'userId', 'carId', 'totalDayOrHr', 'subtotal', 'type', 'brand', 'rateText', 'cancelReason',
         'taxPer', 'taxAmt', 'oTotal', 'pMethodId', 'postId', 'pickOtp', 'dropOtp', 'commission',
         'transactionId', 'bookStatus', 'pickupDate', 'city', 'location', 'carPrice', 'wallAmt',
         'pickupTime', 'returnDate', 'returnTime', 'priceType', 'bookingType', 'exterPhoto', 'interPhoto',
